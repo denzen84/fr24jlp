3bba3d320204af8d950765647f4da32a8e8efe56
Sergey Punkoff <svpmailbox@gmail.com>
2018-12-03 17:39:11 +0300

    Merge. Убрал beautifulsoup

--- fr24jlp.py
+++ fr24jlp.py
@@ -6,21 +6,23 @@ import time
 import fnmatch
 # import urllib3
 # import subprocess
+# import bs4
 
 # -----------------------Задаем параметры---------------------
 
 # Адрес fr24feed
-#addr = 'http://192.168.64.128:8754/flights.json'
-addr = 'http://flangeneer.sknt.ru:8754/flights.json'
+addr = 'http://192.168.64.128:8754/flights.json?time={0}'
+# addr = 'http://flangeneer.sknt.ru:8754/flights.json?time={0}'
 
 # Выборка необходимых позывных
 mask_callsign = {
-    'RSD*':'В небе Правительственный борт',
-    'GZP*':'Борт Газпромавиа',
-    'AFL*':'Рейс Аэрофлот',
-    'PBD*':'Борт ПОБЕДЫ',
-    'PBD282':'Кто последний будет улетать из Петрика, выключите в аэропорту свет',
-    'PBD281':'В Петрик летят москвичи',
+    'RSD*': 'В небе Правительственный борт',
+    'GZP*': 'Борт Газпромавиа',
+    'AFL*': 'Рейс Аэрофлот',
+    'SDM*': 'Рейс Россия',
+    'PBD*': 'Борт ПОБЕДЫ',
+    'PBD282': 'Кто последний будет улетать из Петрика, выключите в аэропорту свет',
+    'PBD281': 'В Петрик летят москвичи',
 }
 
 # Выборка необходимых HEX
@@ -34,6 +36,7 @@ ttl_max = 3600
 
 # Путь к скрипту
 script = '/home/denzenarm/bin/./shishkotryas.sh'
+script2 = '/home/denzenarm/bin/./shishkotryas.sh'
 
 # Период сканирования (time to scan)
 tts = 2
@@ -86,16 +89,25 @@ def is_valid_jet(hex, callsign):
     for mask in mask_hex:
         if fnmatch.fnmatch(hex, mask):
             if update_dict_hex(hex):
-                print('{0} \"{1} {2} HEX={3}\"'.format(script, msg, mask_hex[mask], hex))
+                cmd = '{0} \"{1} {2} HEX={3}\"'.format(script, msg, mask_hex[mask], hex)
+                print(cmd)
+                # subprocess.call(cmd, shell=True)
+                # cmd = '{0} \"{1} {2} HEX={3}\"'.format(script2,msg,mask_hex[mask],hex)
+                # subprocess.call(cmd, shell=True)
     for mask in mask_callsign:
         if fnmatch.fnmatch(callsign, mask):
             if update_dict_callsign(hex):
-                print('{0} \"{1} {2} {3}\"'.format(script, msg, mask_callsign[mask], callsign))
+                cmd = '{0} \"{1} {2} {3}\"'.format(script, msg, mask_callsign[mask], callsign)
+                print(cmd)
+                # subprocess.call(cmd, shell=True)
+                # cmd = '{0} \"{1} {2} {3}\"'.format(script2,msg,mask_callsign[mask],callsign)
+                # subprocess.call(cmd, shell=True)
 
 
 if __name__ == '__main__':
     while True:
-        s = requests.get(addr, headers=headers)
+        s = requests.get(addr.format(int(time.time())), headers=headers)
+        # fr24_answer = bs4.BeautifulSoup(s.text, "html.parser")
         flights_json = json.loads(s.text)
         check_ttl()
         for x in flights_json:
