309bdb091580118b0a74b79fbbe9f592eefcf590
denzen84 <dentall@mail.ru>
2018-12-04 09:00:54 +0300

    Почистил код

--- fr24jlp.py
+++ fr24jlp.py
@@ -4,44 +4,37 @@ import requests
 import json
 import time
 import fnmatch
-# import urllib3
-# import subprocess
-# import bs4
+import subprocess
 
 # -----------------------Задаем параметры---------------------
 
 # Адрес fr24feed
 addr = 'http://192.168.64.128:8754/flights.json?time={0}'
-# addr = 'http://flangeneer.sknt.ru:8754/flights.json?time={0}'
 
 # Выборка необходимых позывных
 mask_callsign = {
     'RSD*': 'В небе Правительственный борт',
-    'GZP*': 'Борт Газпромавиа',
+    'GZP*': 'Рейс Газпромавиа',
     'AFL*': 'Рейс Аэрофлот',
-    'SDM*': 'Рейс Россия',
-    'PBD*': 'Борт ПОБЕДЫ',
-    'PBD282': 'Кто последний будет улетать из Петрика, выключите в аэропорту свет',
-    'PBD281': 'В Петрик летят москвичи',
+    'SDM*': 'Рейс Россия'
 }
 
 # Выборка необходимых HEX
 mask_hex = {
     '155*':'В небе SSJ',
-    '154*':'Нынче редкая птица Ту-154',
-    '14A*':'На радаре Як-42',
+    '154*':'Обнаружена нынче редкая птица Ту-154',
+    '14A*':'На радаре Як-42'
 }
 # Через сколько времени считать появление борта новым (секунды)
 ttl_max = 3600
 
-# Путь к скрипту
-script = '/home/denzenarm/bin/./shishkotryas.sh'
-script2 = '/home/denzenarm/bin/./shishkotryas.sh'
+# Путь к скрипту, который будет запущен при совпадении масок
+script = '/home/denzenarm/bin/./tgbot_sendtext.sh'
 
 # Период сканирования (time to scan)
-tts = 2
+tts = 30
 
-msg = ">>:"
+msg = "Радар ULSS7:"
 
 # -----------------------------------------------------------
 
@@ -90,24 +83,18 @@ def is_valid_jet(hex, callsign):
         if fnmatch.fnmatch(hex, mask):
             if update_dict_hex(hex):
                 cmd = '{0} \"{1} {2} HEX={3}\"'.format(script, msg, mask_hex[mask], hex)
-                print(cmd)
-                # subprocess.call(cmd, shell=True)
-                # cmd = '{0} \"{1} {2} HEX={3}\"'.format(script2,msg,mask_hex[mask],hex)
-                # subprocess.call(cmd, shell=True)
+                #print(cmd)
+                subprocess.call(cmd, shell=True)
     for mask in mask_callsign:
         if fnmatch.fnmatch(callsign, mask):
             if update_dict_callsign(hex):
                 cmd = '{0} \"{1} {2} {3}\"'.format(script, msg, mask_callsign[mask], callsign)
-                print(cmd)
-                # subprocess.call(cmd, shell=True)
-                # cmd = '{0} \"{1} {2} {3}\"'.format(script2,msg,mask_callsign[mask],callsign)
-                # subprocess.call(cmd, shell=True)
-
+                #print(cmd)
+                subprocess.call(cmd, shell=True)
 
 if __name__ == '__main__':
     while True:
         s = requests.get(addr.format(int(time.time())), headers=headers)
-        # fr24_answer = bs4.BeautifulSoup(s.text, "html.parser")
         flights_json = json.loads(s.text)
         check_ttl()
         for x in flights_json:
